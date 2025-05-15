8575473b62b2de68a10a008336706eac265c873c
ramdani2005 <ramdanialghifary44@gmail.com>
2025-05-15 14:05:55 +0700

    website

---
+++ PROJEK PROFIL/profil.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const today = new Date();
+  const formattedDate = today.toDateString(); // Example: "Mon May 15 2025"
+  document.getElementById("date").textContent = formattedDate;
+})
+
+window.onload = () => {
+    alert("Haiii! Terima kasih sudah mampir di biodata saya 😊");
+};
